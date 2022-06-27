9ee2346707ef834b53d804900ec6a80b440558c9
alweso <alweso@gmail.com>
2022-06-27 13:17:37 +0200

    small change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and avabbbaaaa to reload.
+          Edit <code>src/App.js</code> and avabbbaaaa to reload small change.
         </p>a
         <a
           className="App-link"

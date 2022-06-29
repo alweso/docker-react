83479f76584069a937bc416d0661342ca25fab4b
apascaru <alweso@gmail.com>
2022-06-29 10:36:31 +0200

    added a few cjaracjters

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and avabbbaaaa to reload small sassas.
+          Edit <code>src/App.js</code> and avabbbaaaa to relaaoad small sassas.
         </p>a
         <a
           className="App-link"

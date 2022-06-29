19ee8d4344d9f4838e3ba0136c04d09a05d5c376
apascaru <alweso@gmail.com>
2022-06-29 11:39:30 +0200

    somethign

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and avabbbaaaa to relaaoad small sassas.
+          Edit <code>src/App.js</code> and avabbbaaaa to relaaoaaaaaaaaaaad small sassas.
         </p>a
         <a
           className="App-link"
